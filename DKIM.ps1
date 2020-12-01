7c9965bec7f2f8b91a874cbcbdc69b00d0c54874
partha1194 <30717449+partha1194@users.noreply.github.com>
2020-12-01 08:26:59 +0530

    Add files via upload

---
+++ DKIM.ps1
@@ -0,0 +1,14 @@
+﻿powershell -ExecutionPolicy ByPass
+Set-ExecutionPolicy RemoteSigned
+$UserCredential=Get-Credential
+$s=New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -credential $UserCredential -Authentication Basic -AllowRedirection
+import-PSSession -session $s
+#Install-Module -Name AzureAD
+#Install-Module MSOnline
+Connect-MsolService -Credential $UserCredential
+Get-MsolDomain
+$dname=Read-Host 'Enter the domain name for which DKIM to be activated ::'
+Get-DkimSigningConfig
+New-DkimSigningConfig -DomainName $dname -Enabled $true
+Get-DkimSigningConfig -Identity $dname | fl
+Set-DkimSigningConfig -Identity $dname -Enabled $true
